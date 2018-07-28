88f0d2013b08a69cbb763a023d136692f3b04397
Jermaine <maine1191@gmail.com>
2018-07-28 12:50:29 -0400

    initial commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import FavsScreen from '../screens/FavsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Search',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const FavsStack = createStackNavigator({
+  Fav: FavsScreen,
+});
+
+FavsStack.navigationOptions = {
+  tabBarLabel: 'Favorites',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+    />
+  ),
+};
+
+   
+
+export default createBottomTabNavigator({
+  HomeStack,
+  FavsStack
+});
